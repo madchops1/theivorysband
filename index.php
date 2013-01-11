00ac6db96007d60c7b86ef0283403f3687490d70
Karl Steltenpohl <karl@webksd.com>
2013-01-11 14:15:12 -0600

    -kjs first commit

---
+++ index.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+		
+		<!-- meta tags -->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		
+		<!-- title of the page -->
+		<title>The Ivorys Official Website</title>
+		
+		<!-- main CSS Stylesheet -->
+		<link href="style_classic.css" rel="stylesheet" type="text/css" />
+		<link href="style_conf.css" rel="stylesheet" type="text/css" />
+        
+        <!-- fancybox stylesheet -->
+		<link rel="stylesheet" href="fancybox/jquery.fancybox-1.3.1.css" type="text/css" media="screen" />
+		
+		<!-- Nivo Slider stylesheet -->
+		<link rel="stylesheet" href="nivo-slider/nivo-slider.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="nivo-slider/themes/default/default.css" type="text/css" media="screen" />
+		
+		<!-- .JS Files -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="js/cufon-yui.js"></script>
+		<script type="text/javascript" src="js/LeagueGothic.font.js"></script>
+		<script type="text/javascript" src="nivo-slider/jquery.nivo.slider.pack.js"></script>
+		<script type="text/javascript" src="js/scripts.js"></script>
+        
+        <!-- fancybox js -->
+		<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.1.pack.js"></script>
+        
+        <script type="text/javascript">
+		
+			$(document).ready(function() {
+				
+				$('.menu').dropDown();
+				//$('#hide-fixed').hideMenu();
+				$//('#showcase').DDShowCase();
+				$('.more').DDShowHidden();
+				
+				$(".lightbox").fancybox({ 'titleShow' : false });
+				
+				Cufon.replace('.menu a, h1, h2, h3, h4, h5, h6', {
+					hover: true
+				});
+
+				
+					
+				
+			});
+
+			/*
+			function instagramLoad(){
+				console.log('iframe loaded');
+				$("iframe ul.media-grid li a").live('click',function(e){
+					e.preventDefault();
+					console.log('clicked');
+				});
+			}
+			*/
+			
+		</script>
+        
+        <!--[if IE 6]>
+		<script src="js/DD_belatedPNG.js"></script>
+		<script> DD_belatedPNG.fix('*'); </script>
+		<link href="ie6.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+		
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-34996752-1']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
+		
+	</head>
+
+	<body>
+    
+    <script type="text/javascript">
+
+
+</script>
+
+		<div id="top_menu">
+        	<ul class="menu">
+                  <!-- <li class="current"><a href="/">Home</a></li> -->
+                  <li><div class="fb-like" data-href="http://facebook.com/TheIvorys" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
+                  <li><a href="https://twitter.com/TheIvorys" class="twitter-follow-button" data-show-count="false">Follow @TheIvorys</a></li>
+              		
+              	  <!--  
+                  <li><a href="portfolio.html">Portfolio</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                  -->
+              
+              </ul>
+              <!-- /#menu -->
+              <div class='social-icons' style='float:left; margin-left:20px; margin-top:2px;'>
+	        		<a class='social youtube' href='http://www.youtube.com/user/TheIvorys' target='_blank'>You Tube</a>
+	        		<a class='social twitter' href='http://twitter.com/theivorys' target='_blank'>Twitter</a>
+	        		<a class='social facebook' href='http://www.facebook.com/TheIvorys' target='_blank'>Facebook</a>
+	        		<a class='social soundcloud' href='http://soundcloud.com/theivorys' target='_blank'>Souncloud</a>
+	        		<a class='social spotify' href='http://open.spotify.com/artist/6ITon1oysmGOeJekhy2SQn' target='_blank'>Spotify</a>
+					<a class='social myspace' href='http://www.myspace.com/theivorys' target='_blank'>MySpace</a>
+					<a class='social itunes' href='http://click.linksynergy.com/fs-bin/stat?id=eH8NOY8fBFE&offerid=78941&type=3&subid=0&tmpid=1826&RD_PARM1=http%253A%252F%252Fphobos.apple.com%252FWebObjects%252FMZStore.woa%252Fwa%252FviewAlbum%253Fid%253D564643765%2526s%253D143441%2526partnerId%253D30' target='_blank'>iTunes</a>
+        		</div>
+              <div id="top_menu_bg"></div>
+        
+        </div>
+        <!-- /#top_menu -->
+    
+    	<!--  <div id="hide-fixed"></div> -->
+        
+    	<div id="fixed-scroll">
+        
+        	<div id="logo"> <!--  <h3><a href="#"></a></h3> --> </div>
+            <!-- /#logo -->
+              
+              <ul class="menu">
+              
+                  <!--  <li class="current"><a href="index-2.html">Home</a></li> -->
+              	  <li><div class="fb-like" data-href="http://facebook.com/TheIvorys" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
+                  <li>
+                  		<a href="https://twitter.com/TheIvorys" class="twitter-follow-button" data-show-count="false">Follow @TheIvorys</a>
+                  		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				  </li>
+                  <!-- 
+                  <li><a href="page.html">About</a>
+                  	<ul>
+                    	<li><a href="page.html">My Work</a></li>
+                    	<li><a href="portfolio.html">Example of</a></li>
+                    	<li><a href="portfolio.html">submenu</a></li>
+                    </ul>
+                  </li>
+                  
+                  <li><a href="portfolio.html">Portfolio</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+              	  -->
+              </ul>
+              <!-- /#menu -->
+
+        		<div class='social-icons' style='margin-bottom:5px;'>
+	        		<a class='social youtube' href='http://www.youtube.com/user/TheIvorys' target='_blank'>You Tube</a>
+	        		<a class='social twitter' href='http://twitter.com/theivorys' target='_blank'>Twitter</a>
+	        		<a class='social facebook' href='http://www.facebook.com/TheIvorys' target='_blank'>Facebook</a>
+	        		<a class='social soundcloud' href='http://soundcloud.com/theivorys' target='_blank'>Souncloud</a>
+	        		<a class='social spotify' href='http://open.spotify.com/artist/6ITon1oysmGOeJekhy2SQn' target='_blank'>Spotify</a>
+					<a class='social myspace' href='http://www.myspace.com/theivorys' target='_blank'>MySpace</a>
+					<a class='social itunes' href='http://click.linksynergy.com/fs-bin/stat?id=eH8NOY8fBFE&offerid=78941&type=3&subid=0&tmpid=1826&RD_PARM1=http%253A%252F%252Fphobos.apple.com%252FWebObjects%252FMZStore.woa%252Fwa%252FviewAlbum%253Fid%253D564643765%2526s%253D143441%2526partnerId%253D30' target='_blank'>iTunes</a>
+        			<br style='clear:both; height:1px;' />
+        		</div>	
+        		
+        	  <div>
+        	  <iframe width="100%" height="450" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F2267962&show_artwork=true"></iframe>
+        	  <!-- <iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F26003186&amp;auto_play=false&amp;show_artwork=true&amp;color=ff7700"></iframe> -->
+        	  </div>
+        
+		</div>
+        <!-- /#fixed-scroll -->
+        
+        <?php include 'showcase.php'; ?>
+        
+        <div id="top_info">
+            <div class="wrapper">
+            	<img src="images/ivoryslogo.png" width="250px" style="margin:0px auto 20px auto" height="" alt="The Ivorys" class="alignleft" />
+            	<!-- <a class='text-logo'>The Ivorys</a> -->
+            	
+            	<br clear="all" />
+            	<!-- 
+            	<div class="slider-wrapper theme-default" style='width:780px; height:315px; overflow:hidden; margin-top:20px;'>
+		            <div id="slider" class="nivoSlider" style='overflow:hidden; height:315px;'>
+		                <img src="images/brendancropped.jpg" data-thumb="images/brendancropped.jpg" alt="" style='' />
+		                <img src="images/croppedbass.jpg" data-thumb="images/croppedbass.jpg" alt="" style='' />
+		                <img src="images/neilcropped.jpg" data-thumb="images/neilcropped.jpg" alt="" style='' />
+		                <img src="images/samcropped.jpg" data-thumb="images/samcropped.jpg" alt="" style='' />
+		            </div>
+		        </div>
+            	-->
+            	<iframe width="600" height="315" src="http://www.youtube.com/embed/videoseries?list=PLj7rYuAZqcZAt5EDKQOf3FmPmN51t23Oa&amp;hl=en_US" frameborder="0" allowfullscreen></iframe>
+            	<!--  <iframe width="600" height="315" src="http://www.youtube.com/embed/vaAre1RKXoo" frameborder="0" allowfullscreen></iframe>
+            	 -->
+            	<br clear="all" />
+            	<!-- 
+            	<p></p>
+           		-->
+            </div>
+            <!-- /.wrapper -->
+        
+        </div>
+        <!-- /#top_info -->
+        
+        <div id="top_info_bg_bottom"></div>
+        <!-- /#top_info_bg_bottom -->
+        
+        <div id="content">
+        
+        	<div class="wrapper">
+                
+                <div class="post_item">
+                
+                    <div class="one">
+						<!-- 
+						<iframe src="http://embedagram.com/e/widget/225572638/?css=http://mysite.com/embedagram.css" scrolling="no" frameborder="0" style="border:none; width:600px; height:144px; overflow:hidden;" allowTransparency="true"></iframe>
+						-->
+						
+						<iframe src="http://www.bandrx.com/facebook/events.php?theme=zuck&page=TheIvorys&bgcolor=ffffff&textcolor=000000&linkcolor=555555&max=5" frameborder="no" scrolling="auto" height="320" width="600"></iframe>
+					</div>
+                        
+                     
+                
+                </div>
+                <!-- /.post_item -->
+            
+            </div>
+            <div class='wrapper'>
+                
+                <div class="post_item">
+                	<?php /* 
+                    <h1 class="title"><a href="blog_entry.html">The Ivorys - Piece of Mind</a></h1>
+            		<!--         
+                    <div class="comments">!<a href="blog_entry.html#comments">32</a></div>
+            		-->
+                    <div class="post_info">
+                        <!-- <div class="left">
+                            <div class="more"></div>
+                        </div -->
+                        <!-- /.left -->
+                        <div class="right">
+                            <div class="date">
+                                <span>2012</span>april
+                            </div>
+                            <!-- /.date -->
+                            <div class="categories">
+                                <span>venue:</span><a href="#">The Metro</a>
+                            </div>
+                            <!-- /.categories -->
+                        </div>
+                        <!-- /.right -->
+                    </div>
+                    <!-- /.post_info -->
+                    */ ?>
+                    <div class="one">
+                    	<iframe width="600" height="400" src="http://www.youtube.com/embed/IQM0zkpGXEY" frameborder="0" allowfullscreen></iframe>
+					</div>
+                        
+                      <!--  
+                    <div class="hidden_content">
+                            
+                            <div class="one_fourth">&nbsp;</div>
+                            
+                            <div class="three_fourths last">
+                            
+                                <p>Cras lacinia auctor nisl. Integer eu tempus ante. Pellentesque eu turpis eget neque venenatis tincidunt. Duis commodo, mi id facilisis congue, tellus mauris mollis tellus, id aliquet nibh tortor dictum odio. Sed porta augue, <a href="#">nibh nec iaculis rhoncus</a> mollis quam laoreet tempus. Duis ac enim ut urna posuere cursus sit amet tincidunt nibh. Curabitur porttitor.</p>
+                            
+                                <p>Pellentesque eu turpis eget neque venenatis tincidunt. Duis commodo, mi id facilisis congue, tellus mauris mollis tellus, id aliquet nibh tortor dictum odio. Sed porta augue mollis quam laoreet tempus. Duis ac enim ut urna posuere cursus sit amet tincidunt nibh. Curabitur porttitor, nibh nec iaculis rhoncus.</p>
+                            
+                                <p>Cras lacinia auctor nisl. Integer eu tempus ante. Pellentesque eu turpis eget neque venenatis tincidunt. Duis commodo, mi id facilisis congue, tellus mauris mollis, <a href="#">nibh nec iaculis rhoncus</a> tellus, id aliquet nibh tortor dictum odio. Sed porta augue mollis quam laoreet tempus. Duis ac enim ut urna posuere cursus sit amet tincidunt nibh. Curabitur porttitor.</p>
+                            
+                                <p>Pellentesque eu turpis eget neque venenatis tincidunt. Duis commodo, mi id facilisis congue, tellus mauris mollis tellus, id aliquet nibh tortor dictum odio. Sed porta augue mollis quam laoreet tempus. Duis ac enim ut urna posuere cursus sit amet tincidunt nibh. Curabitur porttitor, nibh nec iaculis rhoncus.</p>
+                                
+                            </div>
+                        
+                    </div>
+                         /.hidden content -->
+                
+                </div>
+                <!-- /.post_item -->
+                
+                
+                <!-- 
+                <div id="pagination">
+                    <ul>
+                        <li class="current"><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">next</a></li>
+                    </ul>
+                </div>
+                -->
+            
+            </div>
+            <!-- /.wrapper -->
+            
+        </div>
+        <!-- /#content -->
+        
+        <?php /* 
+        <div id="footer_bg_top"></div>
+        <div id="footer">
+        
+        	<div class="wrapper">
+            
+            	<div class="one_third" id="recent_comments">
+                
+                	<h2>Recent Comments:</h2>
+                    
+                    <ul>
+                    
+                    	<li>Yeah, this is an example of a very long comments that I wrote for you to read now</li>
+                    
+                    	<li>Oh no! This is a short comment!</li>
+                    
+                    	<li>Ok, this is a med comment, because we already have long and short.</li>
+                    
+                    </ul>
+                    <!-- /#recent_comments ul -->
+                
+                </div>
+                <!-- /#recent_comments -->
+            
+            	<div class="one_third">
+                
+                	<h2>Search me:</h2>
+                    
+                    <form id="search" action="http://themes.ddwebstudios.net/templates/blink/classic/index.html" method="post">
+                    
+                    	<input type="text" class="search_field left" name="search" />
+                        
+                        <input type="submit" class="submit search_submit" value="Go" />
+                    
+                    </form>
+                    
+                    <p>&nbsp;</p>
+                    
+                    <h2>Find me elsewhere:</h2>
+                    
+                    <ul class="list-links">
+                    
+                    	<li><a href="http://www.orkut.co.in/Main#Profile?uid=4993189625345196010"><img src="assets/orkut.png" width="32" height="32" alt="orkut" /></a></li>
+                    	<li><a href="http://facebook.com/guisalum"><img src="assets/facebook_icon.png" width="32" height="32" alt="facebook" /></a></li>
+                    	<li><a href="#"><img src="assets/myspace.png" width="32" height="32" alt="myspace" /></a></li>
+                    	<li><a href="#"><img src="assets/rss.png" width="32" height="32" alt="rss" /></a></li>
+                    	<li><a href="#"><img src="assets/twitter.png" width="32" height="32" alt="twitter" /></a></li>
+                    
+                    </ul>
+                
+                </div>
+                <!-- /.one_third -->
+            
+            	<div class="one_third last">
+                
+                	<h2>What you don't know</h2>
+                    
+                    <p><img src="assets/footer_img.png" width="68" height="72" alt="img" class="alignleft" />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula augue vel ante pharetra in pretium leo mattis. Ut eu felis metus, eu ultricies urna. Maecenas lobortis, ligula vitae dignissim pulvinar, sem ligula sodales lacus, in iaculis velit dolor ac tortor. Ut egestas enim orci. Integer sodales ornare odio;</p>
+                
+                </div>
+                <!-- /.one_third .last -->
+            
+            </div>
+            <!-- /.wrapper -->
+        
+        </div>
+        <!-- /#footer -->
+        
+        
+        <div id="copyright_bg_top"></div>
+        */ ?>
+        <div id="copyright">
+        
+        	<div class="wrapper">
+            
+            	<div class="left">
+                
+                	<p>Powered by <a href='http://www.karlsteltenpohl.com' target='_blank'>KJS</a></p>
+                
+                </div>
+                <!-- /.left -->
+                
+                <div class="right">
+                
+                	<p>The Ivorys &copy; 2012, All rights reserved.</p>
+                
+                </div>
+                <!-- /.right -->
+            
+            </div>
+            <!-- /.wrapper -->
+        
+        </div>
+        <!-- /#copyright -->
+          	
+          	<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>  
+	</body>
+    
+
+</html>
