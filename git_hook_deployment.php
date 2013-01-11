155372caa2730ecce6c30dad9826fda72c812aa1
Karl Steltenpohl <karl@webksd.com>
2013-01-11 15:48:04 -0600

    added git deployment hook

--- git_hook_deployment.php
+++ git_hook_deployment.php
@@ -1,6 +1,20 @@
 <?php
 if($_POST['payload']){
-  shell_exec( 'sudo cd /home/theivorys1/public_html/ && sudo git reset --hard HEAD && sudo git pull origin master' );
-}
+  shell_exec('sudo cd /home/theivorys1/public_html/ && sudo git reset --hard HEAD && sudo git pull origin master');
+  
+  // The message
+  $message = "  Codebase deployed to TheIvorysBand.com \r\n
+                Date:".date("m/d/Y h:i:s A")."\r\n
+                Status: Successfull\r\n
+                \r\n
+                --------------------------------------------------
+                \r\n
+                ".$POST['payload']."\r\n";
 
+  // In case any of our lines are larger than 70 characters, we should use wordwrap()
+  $message = wordwrap($message, 70, "\r\n");
+  
+  // Send
+  @mail('karl@webksd.com', 'TheIvorysBand.com Deployed Successfully '.date("m/d/Y h:i:s A").'', $message);
+}
 ?>
