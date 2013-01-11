49f8e05a822b46da414d09c06e8157cf47ea7174
root <root@ip-173-201-187-105.ip.secureserver.net>
2013-01-11 14:14:15 -0700

    first remote commit

---
+++ showcase.php
@@ -0,0 +1,319 @@
+<?php 
+
+?>
+
+
+        <div id="showcase">
+        	<?php /* ?>
+        	<div id="showcase_wrapper">
+        
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_001.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        
+                        
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                         Sed rutrum, diam at molestie fringilla, arcu libero vest
+                         ibulum sapien, non semper elit lorem vitae turpis. Nam rho
+                         ncus augue nec odio commodo ornare. Suspendisse dui nisi, mat
+                         tis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus e
+                         u velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus.
+                          </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_002.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_003.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_004.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_005.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_006.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_007.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_008.gif" width="60px" height="60px" alt="Gratitude" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_009.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_010.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_001.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_002.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_003.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_004.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_005.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+                <div class="showcase_item">
+                
+                    <a href="assets/portfolio_001-1.jpg" class="lightbox"><img src="assets/showcase_006.gif" width="60px" height="60px" alt="image" /></a>
+                    
+                    <div class="hidden">
+                    
+                        <h1><a href="portfolio.html">My project title</a></h1>
+                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum, diam at molestie fringilla, arcu libero vestibulum sapien, non semper elit lorem vitae turpis. Nam rhoncus augue nec odio commodo ornare. Suspendisse dui nisi, mattis eu lobortis id, aliquet vitae ipsum. Curabitur quis lacus eu velit lacinia eleifend. Donec dapibus sagittis turpis sed rhoncus. </p>
+                        
+                        <p><a href="#">Read More</a></p>
+                    
+                    </div>
+                    <!-- /.hidden content -->
+                
+                </div>
+                <!-- /.showcase_item -->
+            
+            </div>
+            <!-- /#showcase_wrapper -->
+        	<?php */ ?>
+        	
+        	<iframe src="http://widget.stagram.com/in/theivoryschi/?s=60&w=1&h=20&b=0&p=0" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:60px; height: 100%;" onload="instagramLoad()"></iframe> <!-- Webstagram - web.stagram.com -->
+        	
+        </div>
+        <!-- /#showcase -->
+         
+        <?php /*
+        <div id="showcase_arrow_up"></div>
+        <div id="showcase_arrow_down"></div>
+        <!-- showcase arrows --> */ ?>
