20beac7fb2de18a03581a3d23eaf5e04cf6d7429
Karl Steltenpohl <karl@webksd.com>
2013-01-14 12:33:41 -0600

    -kjs

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		
 		<!-- title of the page -->
-		<title>The Ivorys Official Website - TEST CHANGE 000000103</title>
+		<title>The Ivorys Official Website - TEST CHANGE 000000104</title>
 		
 		
 		<!-- test change -->
