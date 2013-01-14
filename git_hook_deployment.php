00bc3f289bbb2c438b5bfb4bd9046bcf5500b2b6
Karl Steltenpohl <karl@webksd.com>
2013-01-14 14:38:11 -0600

    -kjs

--- git_hook_deployment.php
+++ git_hook_deployment.php
@@ -1,11 +1,13 @@
 <?php
-//if(isset($_POST['payload'])){
+// Payload comes from github
+if(isset($_POST['payload'])){
+  // Sleep for 15 sec just for safety
   sleep(15);
   /* I have given the apache user on the server an ssh key pair for git
-   * The .git directory in /ivorys1/public_html/ is chmoded to 0777 so 
+   * The apache user owns the .git directory in /ivorys1/public_html/ and 
+   * has read/write permission on the public_html folder
    * the apache user should be able to pull from git!
    */
-
   function sendMessage($output){
     $message = "TheIvorysBand.com Deployment Report
             Date:".date("m/d/Y h:i:s A")."
@@ -15,19 +17,8 @@
     @mail('karl@webksd.com', 'TheIvorysBand.com Deployment Report '.date("m/d/Y h:i:s A").'', $message);
     @mail("6302175813@txt.att.net",$subject,$message,"From: deployment@theivorysband.com");
   }
-
- 
-  
-  //$output = array();
-  //exec('git pull origin master',$output,$return);
-  //echo passthru('git pull origin master',$return);
-  //$return = shell_exec('git pull origin master 2>&1');
-  //shell_exec('git reset --hard HEAD');
-  //sleep(5);
   $return = shell_exec('git pull origin master 2>&1');
   echo "<pre>$return</pre>";
   sendMessage($return);
- 
-
-//}
+}
 ?>
