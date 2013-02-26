7bee0466296c99f12ae7630da9cfeb4a498993a8
Karl Steltenpohl <karl@webksd.com>
2013-02-25 19:13:25 -0600

    update to soundcloud player, now connected to spotlight

--- index.php
+++ index.php
@@ -161,8 +161,15 @@
         		</div>	
         		
         	  <div>
+        	  <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fusers%2F3673469"></iframe>
+        	  
+        	  <!-- 
         	  <iframe width="100%" height="450" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F2267962&show_artwork=true"></iframe>
-        	  <!-- <iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F26003186&amp;auto_play=false&amp;show_artwork=true&amp;color=ff7700"></iframe> -->
+        	  -->
+        	  
+        	  <!-- 
+        	  <iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F26003186&amp;auto_play=false&amp;show_artwork=true&amp;color=ff7700"></iframe>
+        	  -->
         	  </div>
         
 		</div>
